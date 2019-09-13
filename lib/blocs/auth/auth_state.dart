9acb7bef4f03640b561fc21394e9407ed46e3ea7
Iampato <858wpwaweru@gmail.com>
2019-09-13 20:54:45 +0300

    Added auth states

---
+++ lib/blocs/auth/auth_state.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class AuthState extends Equatable {
+  AuthState([List props = const <dynamic>[]]) : super(props);
+}
+
+//Authenticated state
+class Authenticated extends AuthState {
+
+  @override
+  String toString() => 'Authenticated ';
+}
+
+class Unauthenticated extends AuthState {
+
+  @override
+  String toString() => 'Unauthenticated';
+}
