1a9c101786b87afa0de8ffb0941d998f79b40984
Iampato <858wpwaweru@gmail.com>
2019-09-13 20:52:47 +0300

    Added authentication bloc

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:devfest19/blocs/user_repository.dart';
+import './bloc.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserRepository _userRepository;
+
+  AuthBloc({@required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
+  @override
+  AuthState get initialState => Unauthenticated();
+
+  @override
+  Stream<AuthState> mapEventToState(
+    AuthEvent event,
+  ) async* {
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is LoggedIn) {
+      yield* _mapLoggedInToState();
+    } else if (event is LoggedOut) {
+      yield* _mapLoggedOutToState();
+    }
+  }
+
+  Stream<AuthState> _mapAppStartedToState() async* {
+    try {
+      final isSignedIn = await _userRepository.isSignedIn();
+      if (isSignedIn) {
+        yield Authenticated();
+      } else {
+        yield Unauthenticated();
+      }
+    } catch (_) {
+      yield Unauthenticated();
+    }
+  }
+
+  Stream<AuthState> _mapLoggedInToState() async* {
+    yield Authenticated( );
+  }
+
+  Stream<AuthState> _mapLoggedOutToState() async* {
+    yield Unauthenticated();
+    _userRepository.signOut();
+  }
+
+}
