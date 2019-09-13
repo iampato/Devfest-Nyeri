559685cca69ac1e218cdbe11e45438cbffb7b24f
Iampato <858wpwaweru@gmail.com>
2019-09-13 20:54:17 +0300

    Added authentication events

---
+++ lib/blocs/auth/auth_event.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class AuthEvent extends Equatable {
+  AuthEvent([List props = const <dynamic>[]]) : super(props);
+}
+
+class AppStarted extends AuthEvent {
+  @override
+  String toString() => 'AppStarted';
+}
+
+class LoggedIn extends AuthEvent {
+  @override
+  String toString() => 'LoggedIn';
+}
+
+class LoggedOut extends AuthEvent {
+  @override
+  String toString() => 'LoggedOut';
+}
