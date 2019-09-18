98a912f5b89d7ba869d3fb0401d574b08daafa74
Iampato <858wpwaweru@gmail.com>
2019-09-18 15:44:55 +0300

    Added firebase firestore

---
+++ lib/blocs/session/session_repository.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:devfest19/blocs/session/session_model.dart';
+import 'session_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class SessionRepository {
+
+  Stream<List<Session>> sessions();
+
+}
+class FirebaseTodosRepository implements SessionRepository {
+  final sessionCollection = Firestore.instance.collection('Session');
+
+  @override
+  Stream<List<Session>> sessions() {
+    return sessionCollection.snapshots().map((snapshot) {
+      return snapshot.documents
+          .map((doc) => Session.fromEntity(SessionEntity.fromSnapshot(doc)))
+          .toList();
+    });
+  }
+}
