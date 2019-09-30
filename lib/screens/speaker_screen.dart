0feb5ba822ccd00cde3f91cff3df730b31bb65a8
Iampato <858wpwaweru@gmail.com>
2019-09-30 20:40:24 +0300

    Wired in the speaker bloc

--- lib/screens/speaker_screen.dart
+++ lib/screens/speaker_screen.dart
@@ -1,4 +1,7 @@
+import 'package:devfest19/blocs/speaker/bloc.dart';
+import 'package:devfest19/blocs/speaker/speaker_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SpeakerScreen extends StatelessWidget {
   @override
@@ -8,83 +11,90 @@ class SpeakerScreen extends StatelessWidget {
           title: Text("Speakers"),
           elevation: 0.0,
         ),
-        body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, i) {
-            return Padding(
-              padding: const EdgeInsets.only(top: 2.5),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Flexible(
-                        flex: 1,
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints.expand(
-                            height: MediaQuery.of(context).size.height * 0.2,
-                            width: MediaQuery.of(context).size.width * 0.3,
-                          ),
-                          child: Image.asset(
-                            "assets/images/tile.jpg",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        flex: 3,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Patrick Waweru",
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.w600)),
-                            Opacity(
-                              opacity: 0.8,
-                              child: Text(
-                                "Software Engineer Andela Ke",
-                                style: TextStyle(fontSize: 14),
-                              ),
-                            ),
-                            Wrap(
-                              spacing: 2.5,
-                              children: <Widget>[
-                                Chip(
-                                  backgroundColor: Colors.green,
-                                  label: Text("Android"),
-                                ),
-                                Chip(
-                                  backgroundColor: Colors.pink,
-                                  label: Text("Django"),
-                                ),
-                                Chip(
-                                  backgroundColor: Colors.amber,
-                                  label: Text("Firebase"),
-                                ),
-                                Chip(
-                                  backgroundColor: Colors.amber,
-                                  label: Text("C++"),
-                                ),
-                                Chip(
-                                  backgroundColor: Colors.amber,
-                                  label: Text("Firebase"),
-                                ),
-                                Chip(
-                                  backgroundColor: Colors.amber,
-                                  label: Text("C++"),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  Divider()
-                ],
-              ),
-            );
+        body: BlocProvider<SpeakerBloc>(
+          builder: (context) {
+            return SpeakerBloc(speakerRepository: FirebaseTodosRepository())
+              ..dispatch(LoadingSpeaker());
           },
+          child: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, i) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 2.5),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints.expand(
+                              height: MediaQuery.of(context).size.height * 0.2,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                            ),
+                            child: Image.asset(
+                              "assets/images/tile.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Patrick Waweru",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w600)),
+                              Opacity(
+                                opacity: 0.8,
+                                child: Text(
+                                  "Software Engineer Andela Ke",
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ),
+                              Wrap(
+                                spacing: 2.5,
+                                children: <Widget>[
+                                  Chip(
+                                    backgroundColor: Colors.green,
+                                    label: Text("Android"),
+                                  ),
+                                  Chip(
+                                    backgroundColor: Colors.pink,
+                                    label: Text("Django"),
+                                  ),
+                                  Chip(
+                                    backgroundColor: Colors.amber,
+                                    label: Text("Firebase"),
+                                  ),
+                                  Chip(
+                                    backgroundColor: Colors.amber,
+                                    label: Text("C++"),
+                                  ),
+                                  Chip(
+                                    backgroundColor: Colors.amber,
+                                    label: Text("Firebase"),
+                                  ),
+                                  Chip(
+                                    backgroundColor: Colors.amber,
+                                    label: Text("C++"),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider()
+                  ],
+                ),
+              );
+            },
+          ),
         ));
   }
 }
