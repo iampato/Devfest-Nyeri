adc4724564d28f46041c572e8c1fb00a844b9c36
Iampato <858wpwaweru@gmail.com>
2019-10-02 21:45:03 +0300

    Rewrote the entire bloc

--- lib/blocs/theme/theme_state.dart
+++ lib/blocs/theme/theme_state.dart
@@ -1,17 +1,46 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 @immutable
-class ThemeState extends Equatable{
-  final ThemeData themeData;
+abstract class ThemeState extends Equatable {
+  ThemeState([Iterable props]) : super(props);
 
-  ThemeState({
-    this.themeData
-  }):super([themeData]);
+  /// Copy object for use in action
+  ThemeState getStateCopy();
+}
 
-  
+/// UnInitialized
+class UnThemeState extends ThemeState {
+  @override
+  String toString() => 'UnThemeState';
 
   @override
-  String toString() => 'ThemeState'; 
+  ThemeState getStateCopy() {
+    return UnThemeState();
+  }
+}
+
+/// Initialized
+class InThemeState extends ThemeState {
+  @override
+  String toString() => 'InThemeState';
+
+  @override
+  ThemeState getStateCopy() {
+    return InThemeState();
+  }
+}
+
+class ErrorThemeState extends ThemeState {
+  final String errorMessage;
+
+  ErrorThemeState(this.errorMessage);
+  
+  @override
+  String toString() => 'ErrorThemeState';
+
+  @override
+  ThemeState getStateCopy() {
+    return ErrorThemeState(this.errorMessage);
+  }
 }
